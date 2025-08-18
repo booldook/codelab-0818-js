302742bee03285fed4fba4fb26faa1358c729201
booldook <booldook@gmail.com>
2025-08-18 16:12:37 +0900

    자바스크립트 기초

---
+++ js/js01.js
@@ -0,0 +1,17 @@
+/* 
+- 프로그래밍 언어
+대상: 객체(Object, Class)
+  속성: 변수 (const, variable) -> abc
+  행동: 함수 (method, function) -> abc(...)
+- 자바스크립트: 브라우저에서 동작하는 프로그래밍 언어
+window > head : Browser
+document == body : Brower의 표현영역
+alert, confirm, prompt등 window객체가 가지고 있는 함수를 내장함수라 한다.
+
+- 언어
+  - 변수, 함수, 제어, 반복
+*/
+alert("Hello World");
+console.log("Hello World");
+var userName = prompt("너의 이름은?");
+confirm("당신의 이름은 " + userName + " 입니까?");
