2d1933f7739a4a61e92c4f971d6d6f65d9a8ab65
booldook <booldook@gmail.com>
2025-08-19 15:17:16 +0900

    배열

---
+++ js/js03.js
@@ -0,0 +1,20 @@
+/**
+ * JS Array
+ * 배열
+ * 배열변수의 index를 부여하고  변수[Index]에 값을 저장한다.
+ */
+
+// 배열선언
+// var arr = [];
+// var arr = new Array();
+
+function onScore() {
+  var stdNames = ["홍길동", "홍길순", "홍길만"]; // studentsNames[0] 홍길동
+  var stdKors = [95, 65, 100];
+  var stdEngs = [80, 55, 95];
+  var stdMaths = [85, 75, 85];
+  var stdTotal = [];
+  var stdAvg = [];
+  var tbl = document.getElementById("stdTbl");
+  console.log(tbl);
+}
