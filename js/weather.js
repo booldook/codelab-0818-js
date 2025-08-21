c1a4b2de84a9502fe17bef9ea21fb646de1f279c
booldook <booldook@gmail.com>
2025-08-21 18:30:12 +0900

    weather API

---
+++ js/weather.js
@@ -0,0 +1,39 @@
+const apiKey = "d965b9b0eacc39a576dc0027920741f8";
+const reqUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+function getCoords() {
+  return new Promise((resolve) => {
+    let defaultLat = 37.566535;
+    let defaultLon = 126.9779692;
+    navigator.geolocation.getCurrentPosition(
+      (res) => {
+        resolve({
+          lat: res?.coords?.latitude ?? defaultLat,
+          lon: res?.coords?.longitude ?? defaultLon,
+        });
+      },
+      (err) => {
+        console.log(err);
+        resolve({ lat: defaultLat, lon: defaultLon });
+      }
+    );
+  });
+}
+async function getWeather(lat, lon) {
+  // const req = `${reqUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  // const { data } = await axios.get(req);
+  const params = { lat, lon, appid: apiKey, units: "metric" };
+  const { data } = await axios.get(reqUrl, { params });
+  return data;
+}
+
+async function init() {
+  const { lat, lon } = await getCoords();
+  const weather = await getWeather(lat, lon);
+  console.log(weather);
+}
+
+window.addEventListener("load", init);
+
+// QueryString(params)
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
